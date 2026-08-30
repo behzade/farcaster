69f1bcb0e74791f958b8785461468a2919736a25
Behzad <ehsani.eof@gmail.com>
2026-08-30 18:30:10 +0330

    refactor(access): narrow network facade

--- src/modules/access/network.rs
+++ src/modules/access/network.rs
@@ -1,7 +1,3 @@
 pub(crate) use super::adapter::network::{
     append_app_proxy_environment, configuration, validate_app_proxy,
 };
-pub(crate) use super::contract::NetworkConfiguration;
-pub(crate) use super::core::network::{
-    allowed_network_hosts, base_loopback_port_allowed, base_network_host_allowed, loopback_ports,
-};
